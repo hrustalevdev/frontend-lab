08acfaaf155c0714cdd62648d17dc9452c24811a
Timur Khrustalyov <hrustalevdev@gmail.com>
2024-10-08 21:39:01 +0300

    Add base `scss-loader`

--- config/webpack/loaders.ts
+++ config/webpack/loaders.ts
@@ -1,15 +1,18 @@
 import webpack from "webpack";
 
-type Loader = webpack.RuleSetRule
+type Loader = webpack.RuleSetRule;
 
 export function getLoaders(): Loader[] {
-  const tsLoader: Loader = {
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-    };
+  const ts: Loader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
 
-  return [
-    tsLoader
-  ]
+  const scss: Loader = {
+    test: /\.s[ac]ss$/i,
+    use: ["style-loader", "css-loader", "sass-loader"],
+  };
+
+  return [ts, scss];
 }
--- src/features/counter/index.tsx
+++ src/features/counter/index.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 
+import "./styles.scss";
+
 export function Counter({ className }: { className?: string }) {
   const [count, setCount] = useState(0);
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button className="button" onClick={() => setCount(count + 1)}>
+        +
+      </button>
+      <button className="button" onClick={() => setCount(count - 1)}>
+        -
+      </button>
     </div>
   );
 }
